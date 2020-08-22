565e09a45074fd13ac414737836cfcc5e92d40ca
shujinarazaki <shujinarazaki@protonmail.com>
2020-08-22 15:39:03 +0900

    Pos: implement `neighbors` iterator
    	modified:   src/pos.rs

--- src/pos.rs
+++ src/pos.rs
@@ -1,6 +1,9 @@
 use {
     crate::{cell::Cell, RANGE},
-    std::ops::{Add, Neg},
+    std::{
+        iter::{IntoIterator, Iterator},
+        ops::{Add, Neg},
+    },
 };
 
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
@@ -19,11 +22,39 @@ impl Add for Pos {
     }
 }
 
+pub struct Neighbors<'a> {
+    base: Pos,
+    around: &'a [Pos],
+    index: usize,
+}
+
+impl<'a> Iterator for Neighbors<'a> {
+    type Item = Pos;
+    fn next(&mut self) -> Option<Self::Item> {
+        for i in self.index..self.around.len() {
+            if let Some(res) = (self.base + self.around[i]).valid() {
+                self.index = i + 1;
+                return Some(res);
+            }
+        }
+        None
+    }
+}
+
 impl Pos {
     /// constructor without range check
     pub fn at(i: isize, j: isize) -> Pos {
         Pos { i, j }
     }
+    /// into iterator
+    pub fn neighbors<'a>(self, around: &'a [Pos]) -> Neighbors<'a> {
+        Neighbors {
+            base: self,
+            around,
+            index: 0,
+        }
+    }
+
     /// set a digit
     pub fn state(self, digit: usize, on: bool) -> Cell {
         Cell {
@@ -48,5 +79,29 @@ impl Pos {
         } else {
             None
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_around() {
+        assert_eq!(RANGE, 25);
+        let kings_moves = vec![
+            Pos::at(-1, 0),
+            Pos::at(-1, 1),
+            Pos::at(0, 1),
+            Pos::at(1, 1),
+            Pos::at(1, 0),
+            Pos::at(1, -1),
+            Pos::at(0, -1),
+            Pos::at(-1, -1),
+        ];
+        assert_eq!(Pos::at(1, 1).neighbors(&kings_moves).count(), 3);
+        assert_eq!(Pos::at(1, 2).neighbors(&kings_moves).count(), 5);
+        assert_eq!(Pos::at(2, 2).neighbors(&kings_moves).count(), 8);
+        assert_eq!(Pos::at(25, 25).neighbors(&kings_moves).count(), 3);
     }
 }
